17a0a5a6474da25cfb359060a9ff3892dddc7532
NoClaus <support@marketer.consulting>
2022-03-26 09:46:50 +0000

    Fix bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
+import 'bootstrap/js/src/collapse';
+import 'bootstrap/js/src/dropdown';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 require("stylesheets/application.scss")
 
 Rails.start()
