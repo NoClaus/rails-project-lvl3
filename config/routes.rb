7cd694c0c5a3a63d351b31dae3d660c0b80758a8
NoClaus <support@marketer.consulting>
2022-03-26 09:02:18 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@
 Rails.application.routes.draw do
   post '/auth/:provider', to: 'auth#request', as: :auth_request
   get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
+  resources :sessions, only: :destroy
 
   scope module: :web do
-    resources :sessions, only: :destroy
     root 'bulletins#index'
     resources :bulletins do
       member do
