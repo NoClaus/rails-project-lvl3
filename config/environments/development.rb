435f9d5295e665006b73a2ce0bb451852f467308
NoClaus <support@marketer.consulting>
2022-03-26 08:57:48 +0000

    Fix routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << '7ef8699824884fa5849428d5bffa136a.vfs.cloud9.us-east-2.amazonaws.com'
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  post '/auth/:provider', to: 'auth#request', as: :auth_request
+  get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
+
   scope module: :web do
-    post '/auth/:provider', to: 'auth#request', as: :auth_request
-    get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
     resources :sessions, only: :destroy
-    
     root 'bulletins#index'
     resources :bulletins do
       member do
