c62c4689a7a55d164e897573a4826e747ae130d7
NoClaus <support@marketer.consulting>
2022-03-26 09:28:36 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post '/auth/:provider', to: 'auth#request', as: :auth_request
-  get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
-  resources :sessions, only: :destroy
-
   scope module: :web do
     root 'bulletins#index'
+    
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    match 'auth/:provider/callback', to: 'auth#callback', via: %i[get post], as: :callback_auth
+
+    resource :session, only: :destroy
     resources :bulletins do
       member do
         patch 'to_moderate'
