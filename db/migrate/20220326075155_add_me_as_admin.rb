6c54a9a068d9b26cd9e61588925dc5c95b9dc9b0
NoClaus <support@marketer.consulting>
2022-03-26 07:58:02 +0000

    Fix adding admin user

--- db/migrate/20220326075155_add_me_as_admin.rb
+++ db/migrate/20220326075155_add_me_as_admin.rb
@@ -1,5 +1,5 @@
 class AddMeAsAdmin < ActiveRecord::Migration[6.1]
   def change
-     User.find_or_create_by(email: 'ralko.stanislav@gmail.com', name: 'NoClaus').update(admin: true)
+     User.find_or_create_by(email: 'ralko.stanislav@gmail.com').update(admin: true)
   end
 end
